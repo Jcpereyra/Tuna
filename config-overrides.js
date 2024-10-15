2b2223ee05fe7c4c595f0d8ea64fa03849eac37f
Jcpereyra <133118796+Jcpereyra@users.noreply.github.com>
2024-10-15 18:52:09 +0200

    Delete config-overrides.js

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-module.exports = function override(config, env) {
-    // Add the crypto polyfill
-    config.resolve.fallback = {
-      crypto: require.resolve('crypto-browserify'),
-    };
-    return config;
-  };
